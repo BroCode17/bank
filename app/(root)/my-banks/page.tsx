a1998a5a2f536f795c98f313c547bf53fddb2fde
Ebenezer Frimpong <ebenezerfrimpong17@gmail.com>
2024-05-04 11:19:20 -0400

    Implement the complete layout

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyBanks = () => {
+  return (
+    <div>
+      Mybank
+    </div>
+  )
+}
+
+export default MyBanks
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const HomePage = () => {
+  const loggedIn = {firstName: 'Brocode', lastName: 'Ebenezer', email: 'lifeislesson@hotmail.com'}
+
   return (
-    <div>
-      HomePage
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className="home-header">
+            <HeaderBox
+                type="greeting"
+                title="welcome"
+                user={loggedIn?.firtName || 'Guest'}
+                subtext="Access and manage your account and transactions"
+            />
+
+            {/* Total Balance Box */}
+            <TotalBalanceBox
+                accounts={[]}
+                totalBanks = {1}
+                totalCurrentBalance={1250.45}
+            />
+        </header>
+        Recent transactions
+      </div>
+      {/* Will fix this latter */}
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50},{currentBalance: 550.50}]}/>
+    </section>
   )
 }
 
